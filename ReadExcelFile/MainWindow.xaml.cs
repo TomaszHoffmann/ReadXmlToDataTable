6e1d0ad8cf1393534a8ccf8a9c3d3e5129a4eefc
Tomasz Hoffmann <tomaszhoffmann0@gmail.com>
2019-04-23 09:08:13 +0200

    Dodaj pliki projektów.

---
+++ ReadExcelFile/MainWindow.xaml.cs
@@ -0,0 +1,173 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+using System.Data;
+
+using System.Xml;
+using System.Xml.Linq;
+using System.IO;
+using Path = System.IO.Path;
+
+namespace ReadXmlFile
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+
+        public DataTable dt = new DataTable();
+        public void makeDataTable()
+        {
+            dt.Columns.Add("Paczka", typeof(string));
+            dt.Columns.Add("Stojak", typeof(string));
+            dt.Columns.Add("Sztuk w przegrodzie", typeof(string));
+            dt.Columns.Add("Rama", typeof(string));
+            dt.Columns.Add("Skrzydło", typeof(string));
+            dt.Columns.Add("Inne", typeof(string));
+
+
+            dtGrid.ItemsSource = dt.DefaultView;
+
+        }
+              private void btnOpen_Click(object sender, RoutedEventArgs e)
+              {
+                  OpenFileDialog openfile = new OpenFileDialog();
+                  openfile.DefaultExt = ".xml";
+                  openfile.Filter = "(.xml)|*.xml";
+                  //openfile.ShowDialog();
+
+                  var browsefile = openfile.ShowDialog();
+
+                  if (browsefile == true)
+                  {
+
+                      //MessageBox.Show(System.IO.Path.GetDirectoryName(openfile.FileName));
+                      txtFilePath.Text = openfile.FileName;
+                      XmlDocument xmlDoc = new XmlDocument();
+                      xmlDoc.Load(txtFilePath.Text);
+
+                      XElement xmlToSort = XElement.Load(txtFilePath.Text);
+
+                      //MessageBox.Show(xmlToSort.Attribute("Numer").Value.ToString());
+                      /*----------------------------------------------------------------------------*/
+
+                      string source_path = System.IO.Path.GetDirectoryName(openfile.FileName); //@"c:\teren\karol\elwiz\";
+                      string output_path = System.IO.Path.Combine(source_path, "out");
+                      string nr_opt = xmlToSort.Attribute("Numer").Value.ToString(); //"2843";
+
+
+                      SortCiecia.Analyze(source_path, output_path, nr_opt); //grupujemy paczki opt
+
+                      /*----------------------------------------------------------------------------*/
+
+
+
+
+                      makeDataTable();
+
+                      XmlDocument xmlDoc2 = new XmlDocument();
+                      xmlDoc2.Load(System.IO.Path.Combine(output_path, nr_opt + "sorted.xml"));
+                      XmlNodeList nodeList = xmlDoc2.DocumentElement.SelectNodes("Root/Przegroda/Cięcie");
+
+                      XElement xmlToDataTable = XElement.Load(System.IO.Path.Combine(output_path, nr_opt + "sorted.xml"));
+                      // MessageBox.Show(System.IO.Path.Combine(output_path, nr_opt + "sorted.xml"));
+
+
+                      string[] strData = { "", "", "", "", "", "" };
+
+
+                      //MessageBox.Show(level0Element.Attribute("TypProfili").Value.ToString());
+                      foreach (var filepath in Directory.EnumerateFiles(output_path, nr_opt + "*sorted.xml")) // pętla po wszystkich paczkach
+                      {
+                          foreach (XElement level2Element in xmlToDataTable.Elements("Przegroda"))
+
+
+                          {
+
+                              int oscCount = 0;
+                              int skrCount = 0;
+                              int othersCount = 0;
+
+                              strData[1] = level2Element.Attribute("Nazwa").Value.ToString();
+                              strData[2] = level2Element.Attribute("Ile").Value.ToString();
+
+
+
+                              foreach (XElement level3Element in level2Element.Elements("Cięcie"))
+                              {
+
+
+                                  try
+                                  {
+                                      switch (level3Element.Attribute("TypProfEn").Value.ToString())
+                                      {
+
+                                          case "2":
+                                              oscCount += 1; ;
+                                              break;
+                                          case "3":
+                                              skrCount += 1;
+                                              break;
+                                          default:
+                                              othersCount += 1;
+                                              break;
+
+                                      }
+                                  }
+                                  catch (Exception ex)
+                                  {
+
+                                  }
+
+
+                              }
+                              strData[0] = Path.GetFileNameWithoutExtension(filepath).Replace("sorted", "");
+                              strData[3] = (oscCount.ToString());
+                              strData[4] = (skrCount.ToString());
+                              strData[5] = (othersCount.ToString());
+
+
+                              dt.Rows.Add(strData);
+
+                          }
+
+
+                      }
+                  }
+
+
+              }
+    }
+
+
+
+
+
+
+}
+
+
+
---
+++ ReadExcelFile/SortCiecia.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.IO;
+using System.Xml.Linq;
+using System.Collections.Generic;
+
+namespace ReadXmlFile
+{
+  public class MultiValueDictionary<TKey, TValue> : SortedDictionary<TKey, List<TValue>>
+  {
+    public static readonly List<TValue> EmptySet = new List<TValue>();
+    public void Add(TKey key, TValue value)
+    {
+      List<TValue> list;
+      if (!TryGetValue(key, out list))
+        base.Add(key, list = new List<TValue>());
+
+      list.Add(value);
+    }
+
+    public new List<TValue> this[TKey key]
+    {
+      get
+      {
+        List<TValue> list;
+        if (!TryGetValue(key, out list))
+          list = new List<TValue>();
+
+        return list;
+      }
+
+      set { base[key] = value; }
+    }
+  }
+
+  public class SortCiecia
+  {
+    public static void Analyze(string source_path, string output_path, string nr_opt)
+    {
+           
+      if (!Directory.Exists(output_path))
+        Directory.CreateDirectory(output_path);
+
+      foreach (var filepath in Directory.EnumerateFiles(source_path, nr_opt + "*.xml")) // pętla po wszystkich paczkach
+      {
+        string fileNameWithoutExtension = Path.GetFileNameWithoutExtension(filepath);
+        if (fileNameWithoutExtension.EndsWith("z")) continue; // pominięcie pliku od zgrzewarki
+
+        MultiValueDictionary<string, XElement> dict = new MultiValueDictionary<string, XElement>();
+
+        XElement src_root = XElement.Load(filepath);
+        foreach (var ciecie in src_root.Descendants("Cięcie"))
+        {
+          string p = ciecie.Attribute("Przegroda").Value;
+          if (string.IsNullOrEmpty(p)) p = "0";
+
+          dict.Add (ciecie.Attribute("Stojak").Value + "|" + int.Parse(p).ToString("00"), ciecie);
+        }
+
+        XDocument xdoc = new XDocument();
+        XElement out_root = new XElement("Root");
+        foreach (var list in dict)
+        {
+          XElement przegroda = new XElement("Przegroda");
+          przegroda.Add(new XAttribute("Nazwa", list.Key));
+          przegroda.Add(new XAttribute("Ile", list.Value.Count));
+          foreach (var ciecie in list.Value)
+          {
+            przegroda.Add(ciecie);
+          }
+          out_root.Add(przegroda);
+        }
+        xdoc.Add(out_root);
+        xdoc.Save(Path.Combine (output_path, fileNameWithoutExtension + "sorted.xml"));
+      }
+    }
+  }
+}
